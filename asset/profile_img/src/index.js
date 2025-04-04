a007cb5852e7a9bf35cc45d0e72299824fb765bc
eunice <eunyben4@gmail.com>
2025-04-04 17:49:07 +0100

    add:package.json_ignore_node_modules

---
+++ asset/profile_img/src/index.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseApp = initializeApp ({
+    apiKey: "AIzaSyCk1c7S1-SRayX5x402qRt6Kmv1eXTkhJI",
+    authDomain: "alpha-note-274ac.firebaseapp.com",
+    projectId: "alpha-note-274ac",
+    storageBucket: "alpha-note-274ac.appspot.com",
+    messagingSenderId: "683733177535",
+    appId: "1:683733177535:web:3a3312c51a",
+})
